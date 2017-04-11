457246e74d04736fd9790c9c10f4766edbd89f03
João Vitor de Almeida Sabino da Silva <joao.27vitor@hotmail.com>
2017-04-11 10:05:16 -0300

    Create Script.sh
    Nosso primeiro script

---
+++ Script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+clear
+echo "olha MUndo"
+echo "digite um numero"
+Read Num 
+echo "digite outro numero"
+Read Num2
+if (($Num > $Num2)), Then
+  echo "O $num é o maior Número"
+else
+  echo "O @Num2 é o maior Número"
+fi
